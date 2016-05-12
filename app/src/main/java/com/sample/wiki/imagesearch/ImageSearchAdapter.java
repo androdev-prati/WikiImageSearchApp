26f17db74cb54ca1e0645d08ae023f1e45080c9f
androdev-prati <androdev-prati@users.noreply.github.com>
2016-05-12 17:47:22 +0530

    code refactore

--- app/src/main/java/com/sample/wiki/imagesearch/ImageSearchAdapter.java
+++ app/src/main/java/com/sample/wiki/imagesearch/ImageSearchAdapter.java
@@ -23,7 +23,6 @@ public class ImageSearchAdapter extends SimpleCursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         ImageView icon = (ImageView) view.findViewById(R.id.search_result_icon);
-        icon.setTag(cursor.getString(2));
         Picasso.with(context).load(cursor.getString(1)).placeholder(R.mipmap.ic_launcher).into(icon);
         ((TextView) view.findViewById(R.id.search_title)).setText(cursor.getString(2));
         view.setTag(cursor.getString(1));
